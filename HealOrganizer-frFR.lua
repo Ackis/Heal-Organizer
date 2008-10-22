bc78fd84b0c23878ca622f2df69b33c90544aa9c
raegx <raegx@9ab88a1d-1753-40e3-994c-ff0c99ca3dbe>
2008-10-22 01:32:56 +0000

    HealOrganizer: -Support for Ace3.0 -Support for WoW3.0.2 -Small translations from German -> English -No major functionality changes yet -SavedVariables are per profile only right now w/ no interface for it

--- HealOrganizer-frFR.lua
+++ HealOrganizer-frFR.lua
@@ -1,50 +1,48 @@
 -- By mymycracra (http://www.curse-gaming.com/en/profile-14788.html)
-local AceLocale = AceLibrary("AceLocale-2.2"):new("HealOrganizer")
+local L = LibStub("AceLocale-3.0"):NewLocale("HealOrganizer", "frFR", false)
 
-AceLocale:RegisterTranslations("frFR", function()
-    return {
-        ["CLOSE"] = "Ferme",
-        ["RESET"] = "Reset",
-        ["RAID"] = "Raid",
-        ["CHANNEL"] = "Canal",
-        ["DISPEL"] = "Dispel",
-        ["MT"] = "MT",
-        ["HEAL"] = "Soin",
-        ["DECURSE"] = "Decurse",
-        ["REMAINS"] = "Restant",
-        ["ARRANGEMENT"] = "Arrangement",
-        ["BROADCAST"] = "Broadcast",
-        ["OPTIONS"] = "Options",
-        ["STATS"] = "Statistiques",
-        ["PALADIN"] = "Paladin",
-        ["DRUID"] = "Druide",
-        ["PRIEST"] = "Priester",
-        ["SHAMAN"] = "Schamane",
-        ["PALADINS"] = "Paladin",
-        ["DRUIDS"] = "Druides",
-        ["PRIESTS"] = "Pr\195\170tres",
-        ["SHAMANS"] = "Chamans",
-        ["HEALARRANGEMENT"] = "Organisation des soins",
-        ["FFA"] = "ffa", -- was der rest machen darf
-        ["NO_CHANNEL"] = "Vous devez vous connecter sur le canal %q avant d'organiser les soins",
-        ["NOT_IN_RAID"] = "Vous n'etes pas en raid",
-        ["FREE"] = "libre",
-        ["EDIT_LABEL"] = "New label for group %u",
-        ["SHOW_DIALOG"] = "Show/Hide the dialog",
-        ["LABELS"] = "Labels",
-        ["SAVEAS"] = "Save as",
-        ["SET_SAVEAS"] = "Enter a name for the new set",
-        ["SET_DEFAULT"] = "Default",
-        ["SET_CANNOT_DELETE_DEFAULT"] = "You cannot delete the default set",
-        ["SET_CANNOT_SAVE_DEFAULT"] = "You cannot overwrite the default set",
-        ["SET_ALREADY_EXISTS"] = "The Set %q already exists",
-        ["SET_TO_MANY_SETS"] = "You cannot have more than 32 sets",
-        ["AUTOSORT_DESC"] = "Autosort for groups",
-        ["REPLY_NO_ARRANGEMENT"] = "You are not assigned ",
-        ["REPLY_ARRANGEMENT_FOR"] = "You are assigned to %s",
-        ["AUTOFILL"] = "Autofill",
-        ["MSG_HEAL_FOR_ARRANGEMENT"] = "Whisper 'heal' for your assignment.",
-        ["WHISPER"] = "Whisper",
-        ["ARRANGEMENT_FOR"] = "Your arrangement: %s",
-    }
-end)
+if L then
+	L["CLOSE"] = "Ferme";
+	L["RESET"] = "Reset";
+	L["RAID"] = "Raid";
+	L["CHANNEL"] = "Canal";
+	L["DISPEL"] = "Dispel";
+	L["MT"] = "MT";
+	L["HEAL"] = "Soin";
+	L["DECURSE"] = "Decurse";
+	L["REMAINS"] = "Restant";
+	L["ARRANGEMENT"] = "Arrangement";
+	L["BROADCAST"] = "Broadcast";
+	L["OPTIONS"] = "Options";
+	L["STATS"] = "Statistiques";
+	L["PALADIN"] = "Paladin";
+	L["DRUID"] = "Druide";
+	L["PRIEST"] = "Priester";
+	L["SHAMAN"] = "Schamane";
+	L["PALADINS"] = "Paladin";
+	L["DRUIDS"] = "Druides";
+	L["PRIESTS"] = "Pr\195\170tres";
+	L["SHAMANS"] = "Chamans";
+	L["HEALARRANGEMENT"] = "Organisation des soins";
+	L["FFA"] = "ffa"; -- was der rest machen darf
+	L["NO_CHANNEL"] = "Vous devez vous connecter sur le canal %q avant d'organiser les soins";
+	L["NOT_IN_RAID"] = "Vous n'etes pas en raid";
+	L["FREE"] = "libre";
+	L["EDIT_LABEL"] = "New label for group %u";
+	L["SHOW_DIALOG"] = "Show/Hide the dialog";
+	L["LABELS"] = "Labels";
+	L["SAVEAS"] = "Save as";
+	L["SET_SAVEAS"] = "Enter a name for the new set";
+	L["SET_DEFAULT"] = "Default";
+	L["SET_CANNOT_DELETE_DEFAULT"] = "You cannot delete the default set";
+	L["SET_CANNOT_SAVE_DEFAULT"] = "You cannot overwrite the default set";
+	L["SET_ALREADY_EXISTS"] = "The Set %q already exists";
+	L["SET_TO_MANY_SETS"] = "You cannot have more than 32 sets";
+	L["AUTOSORT_DESC"] = "Autosort for groups";
+	L["REPLY_NO_ARRANGEMENT"] = "You are not assigned ";
+	L["REPLY_ARRANGEMENT_FOR"] = "You are assigned to %s";
+	L["AUTOFILL"] = "Autofill";
+	L["MSG_HEAL_FOR_ARRANGEMENT"] = "Whisper 'heal' for your assignment.";
+	L["WHISPER"] = "Whisper";
+	L["ARRANGEMENT_FOR"] = "Your arrangement: %s";
+end
--- HealOrganizer-zhTW.lua
+++ HealOrganizer-zhTW.lua
@@ -1,50 +1,48 @@
 -- By Zisu (http://www.curse-gaming.com/en/profile-122056.html)
-local AceLocale = AceLibrary("AceLocale-2.2"):new("HealOrganizer")
+local L = LibStub("AceLocale-3.0"):NewLocale("HealOrganizer", "zhTW", false)
 
-AceLocale:RegisterTranslations("zhTW", function()
-    return {
-        ["CLOSE"] = "關閉",
-        ["RESET"] = "重置",
-        ["RAID"] = "團隊",
-        ["CHANNEL"] = "頻道",
-        ["DISPEL"] = "驅魔",
-        ["MT"] = "MT",
-        ["HEAL"] = "治療",
-        ["DECURSE"] = "解咒",
-        ["REMAINS"] = "剩餘",
-        ["ARRANGEMENT"] = "安排",
-        ["BROADCAST"] = "廣播",
-        ["OPTIONS"] = "選項",
-        ["STATS"] = "狀態",
-        ["PALADIN"] = "聖騎士",
-        ["DRUID"] = "德魯伊",
-        ["PRIEST"] = "牧師",
-        ["SHAMAN"] = "薩滿",
-        ["PALADINS"] = "聖騎士",
-        ["DRUIDS"] = "德魯伊",
-        ["PRIESTS"] = "牧師",
-        ["SHAMANS"] = "薩滿",
-        ["HEALARRANGEMENT"] = "治療安排",
-        ["FFA"] = "尚未安排", -- was der rest machen darf
-        ["NO_CHANNEL"] = "在廣播治療安排前你必須先加入頻道 %q",
-        ["NOT_IN_RAID"] = "你不在一個團隊中",
-        ["FREE"] = "空",
-        ["EDIT_LABEL"] = "新增標籤到小隊 %u",
-        ["SHOW_DIALOG"] = "顯示/隱藏對話框",
-        ["LABELS"] = "標籤",
-        ["SAVEAS"] = "另存為",
-        ["SET_SAVEAS"] = "輸入新組合的名稱",
-        ["SET_DEFAULT"] = "預設",
-        ["SET_CANNOT_DELETE_DEFAULT"] = "你不能刪除預設組合",
-        ["SET_CANNOT_SAVE_DEFAULT"] = "你不能覆寫預設組合",
-        ["SET_ALREADY_EXISTS"] = "組合 %q 已存在",
-        ["SET_TO_MANY_SETS"] = "你的設定不能超過32組",
-        ["AUTOSORT_DESC"] = "小隊自動排序",
-        ["REPLY_NO_ARRANGEMENT"] = "你沒有被安排到工作",
-        ["REPLY_ARRANGEMENT_FOR"] = "你被安排到 %s",
-        ["AUTOFILL"] = "自動補滿",
-        ["MSG_HEAL_FOR_ARRANGEMENT"] = "使用密語 'heal' 查詢對你的安排工作。",
-        ["WHISPER"] = "密語告知補系對他們的安排工作",
-        ["ARRANGEMENT_FOR"] = "你的安排工作: %s",
-    }
-end)
+if L then
+	L["CLOSE"] = "關閉";
+	L["RESET"] = "重置";
+	L["RAID"] = "團隊";
+	L["CHANNEL"] = "頻道";
+	L["DISPEL"] = "驅魔";
+	L["MT"] = "MT";
+	L["HEAL"] = "治療";
+	L["DECURSE"] = "解咒";
+	L["REMAINS"] = "剩餘";
+	L["ARRANGEMENT"] = "安排";
+	L["BROADCAST"] = "廣播";
+	L["OPTIONS"] = "選項";
+	L["STATS"] = "狀態";
+	L["PALADIN"] = "聖騎士";
+	L["DRUID"] = "德魯伊";
+	L["PRIEST"] = "牧師";
+	L["SHAMAN"] = "薩滿";
+	L["PALADINS"] = "聖騎士";
+	L["DRUIDS"] = "德魯伊";
+	L["PRIESTS"] = "牧師";
+	L["SHAMANS"] = "薩滿";
+	L["HEALARRANGEMENT"] = "治療安排";
+	L["FFA"] = "尚未安排"; -- was der rest machen darf
+	L["NO_CHANNEL"] = "在廣播治療安排前你必須先加入頻道 %q";
+	L["NOT_IN_RAID"] = "你不在一個團隊中";
+	L["FREE"] = "空";
+	L["EDIT_LABEL"] = "新增標籤到小隊 %u";
+	L["SHOW_DIALOG"] = "顯示/隱藏對話框";
+	L["LABELS"] = "標籤";
+	L["SAVEAS"] = "另存為";
+	L["SET_SAVEAS"] = "輸入新組合的名稱";
+	L["SET_DEFAULT"] = "預設";
+	L["SET_CANNOT_DELETE_DEFAULT"] = "你不能刪除預設組合";
+	L["SET_CANNOT_SAVE_DEFAULT"] = "你不能覆寫預設組合";
+	L["SET_ALREADY_EXISTS"] = "組合 %q 已存在";
+	L["SET_TO_MANY_SETS"] = "你的設定不能超過32組";
+	L["AUTOSORT_DESC"] = "小隊自動排序";
+	L["REPLY_NO_ARRANGEMENT"] = "你沒有被安排到工作";
+	L["REPLY_ARRANGEMENT_FOR"] = "你被安排到 %s";
+	L["AUTOFILL"] = "自動補滿";
+	L["MSG_HEAL_FOR_ARRANGEMENT"] = "使用密語 'heal' 查詢對你的安排工作。";
+	L["WHISPER"] = "密語告知補系對他們的安排工作";
+	L["ARRANGEMENT_FOR"] = "你的安排工作: %s";
+end
